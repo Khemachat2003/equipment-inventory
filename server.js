2ae4c3ca307e7be321ac52c2586f60bb20e5e2b2
tikcasnova <tik6507@gmail.com>
2026-03-10 11:50:20 +0700

    fix google credentials env

--- server.js
+++ server.js
@@ -1369,6 +1369,9 @@ app.get("/dashboard", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 
 });
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
