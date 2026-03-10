a57af2e0125549db9264dfd7f8b6ddec3b1c88be
tikcasnova <tik6507@gmail.com>
2026-03-10 13:52:47 +0700

    fix google credentials env

--- server.js
+++ server.js
@@ -58,9 +58,9 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-  secure: process.env.NODE_ENV === "production",
-  sameSite: "none"
-}
+    secure: false,
+    sameSite: "lax"
+  }
 }));
 
 /* =========================
